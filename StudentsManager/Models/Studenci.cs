8967bfc9c0da609de8b47c72e95258ad7ac52105
Bartek Grabowski <bartosz.g.grabowski@gmail.com>
2023-09-11 20:50:43 +0200

    Dodanie adnotacji do klasy Studenci

--- StudentsManager/Models/Studenci.cs
+++ StudentsManager/Models/Studenci.cs
@@ -1,11 +1,23 @@
-﻿namespace StudentsManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsManager.Models
 {
     public class Studenci
     {
         public int IdStudent { get; set; }
+        // Adnotację dotyczą tego co poniżej
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        
         public string IndexNumber { get; set; }
     }
 }
